ab28f1ff1d37d4733af53b1eab2b9e6251af817a
djejkdek-byte <djejkdek@gmail.com>
2026-08-15 11:16:38 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,11 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-#输出so库文件名 libxxxx.so
-LOCAL_MODULE := libtest
-#你的源码文件名
+LOCAL_MODULE := libdrmhook
 LOCAL_SRC_FILES := main.cpp
 
-#编译成动态库so（关键！）
 include $(BUILD_SHARED_LIBRARY)
-
